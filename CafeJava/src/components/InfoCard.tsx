78479653f1dfb05e4e8d8a7ffca45bb674cfbf0c
BrandonCMohammed <64518176+BrandonCMohammed@users.noreply.github.com>
2025-01-15 03:52:03 -0400

    Info card was styled appropriately

--- CafeJava/src/components/InfoCard.tsx
+++ CafeJava/src/components/InfoCard.tsx
@@ -5,10 +5,16 @@ function InfoCard() {
         <a href="https://google.com" target="_blank" className="">
           <div
             className="card bg-custom-black25 custom-max-width "
-            style={{ minHeight: "20rem" }}
+            style={{
+              minHeight: "20rem",
+              backgroundImage:
+                'url("https://chateaudubaffy.education/wp-content/uploads/2024/06/Select-Website-Carousel-1920px-x-800px-Geography-1.jpg")',
+              backgroundSize: "cover",
+              opacity: "0.7",
+            }}
           >
             <div className="card-body d-flex justify-content-center align-items-end">
-              <p className="card-text">
+              <p className="card-text fw-bold" >
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
